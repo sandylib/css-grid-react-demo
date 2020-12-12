7618b5dd82e8e4fa7088f234130936eb5a538f25
Sandy Zhang <sandyzhang@Sandys-MacBook-Pro.local>
2020-12-12 20:22:18 +1100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+
+import styles from './App.module.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className={styles.app}>
+       
+    
+   
+    
+    <div className={styles.content}>
+      
+      </div>
+      <div className={styles.sidebar}>
+
     </div>
+
+    <header className={styles.header}>
+    </header>
+  
+   
+    <div className={styles.footer}>
+
+    </div>
+    </div>
+  
   );
 }
 
---
+++ src/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            home work finish this component
+        </div>
+    )
+}
+
+export default Header
